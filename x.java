418b7841b0bf88acf36af762ad5b2cf0a7b6ca5e
Jahnavi291 <jahnavireddykannapu@gmail.com>
2025-10-19 16:15:33 +0530

    Add files via upload

---
+++ x.java
@@ -0,0 +1,22 @@
+public class x 
+{
+    public static void main(String args[])
+    {
+        int n=10;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(j==i||j==n-i+1)
+                {
+                    System.out.print(" *");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
